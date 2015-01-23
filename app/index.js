3b1e6980d5c72be5f82be528f129b5fb1810c9a7
Miguel Castillo <manchagnu@gmail.com>
2015-01-23 10:43:00 -0500

    Moved app out from generators because that was causing an issue with yo not finding the generator

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
-    this.pkg = require('../../package.json');
+    this.pkg = require('../package.json');
   },
 
   prompting: function () {
@@ -35,6 +35,10 @@ module.exports = yeoman.generators.Base.extend({
       this.fs.copy(
         this.templatePath('_package.json'),
         this.destinationPath('package.json')
+      );
+      this.fs.copy(
+        this.templatePath('_bower.json'),
+        this.destinationPath('bower.json')
       );
       this.fs.copy(
         this.templatePath('_travis.yml'),
